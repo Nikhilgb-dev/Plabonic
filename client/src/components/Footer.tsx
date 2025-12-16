63c7baac28b14a6cfcf02a9c9b3aefbbe493544d
nikhil-dev <nikhilthedev31@gmail.com>
2025-12-16 21:02:08 +0530

    added terms and conditions.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,8 +7,8 @@ const links = [
   { label: 'Careers', to: '#' },
   { label: 'Blog', to: '#' },
   { label: 'Support', to: '#' },
-  { label: 'Privacy', to: '#' },
-  { label: 'Terms', to: '#' }
+  { label: 'Privacy', to: '/tandc_privacy_policy.pdf' },
+  { label: 'Terms', to: '/tandc_privacy_policy.pdf' }
 ]
 
 export default function Footer() {
